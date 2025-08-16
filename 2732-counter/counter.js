c73ffc557eb938c07dc80d3f968744059e9d29b5
Pop.singh <31093814+pop-singh@users.noreply.github.com>
2025-08-16 19:53:16 +0530

    Time: 50 ms (8.36%) | Memory: 53.6 MB (34.65%) - LeetSync

--- 2732-counter/counter.js
+++ 2732-counter/counter.js
@@ -1,20 +1,6 @@
-/**
- * @param {number} n
- * @return {Function} counter
- */
- const fs = require('fs');
-var createCounter = function(n) {
-    
-    return function() {
-        return n++;
+function createCounter(n){
+    let current = n;
+    return function(){
+        return current++;
     };
-};
-process.on('exit', () => {
-    fs.writeFileSync("display_runtime.txt","0");
-})
-/** 
- * const counter = createCounter(10)
- * counter() // 10
- * counter() // 11
- * counter() // 12
- */
+}
