6382b2b20d8cfd9f31219d484f2cd277d3395352
Pop.singh <31093814+pop-singh@users.noreply.github.com>
2025-08-21 00:56:57 +0530

    Time: 8 ms (69.95%) | Memory: 60.8 MB (11.11%) - LeetSync

--- 221-maximal-square/maximal-square.java
+++ 221-maximal-square/maximal-square.java
@@ -10,8 +10,9 @@ class Solution {
             for (int j = 0; j < n; j++) {
                 if (matrix[i][j] == '1') {
                     if (i == 0 || j == 0) {
-                        dp[i][j] = 1; // First row/col, square size is 1 if cell is 1
-                    } else {
+                        dp[i][j] = 1; 
+                        
+                                           } else {
                         dp[i][j] = 1 +
                             Math.min(
                                 Math.min(dp[i-1][j], dp[i][j-1]),
