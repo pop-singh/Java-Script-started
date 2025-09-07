6a4a02791aed69720776624ed5e108f290654be5
Pop.singh <31093814+pop-singh@users.noreply.github.com>
2025-09-07 15:34:10 +0530

    Time: 61 ms (6.01%) | Memory: 55.8 MB (8.98%) - LeetSync

--- 2761-array-reduce-transformation/array-reduce-transformation.js
+++ 2761-array-reduce-transformation/array-reduce-transformation.js
@@ -5,13 +5,5 @@
  * @return {number}
  */
 var reduce = function(nums, fn, init) {
-   let res=init;
-   let sum=0;
-   for(let i=0;i<nums.length;i++)
-   {
-        sum=fn(res,nums[i]);
-       res=sum;
-   }
-   return res;
-    
+   return nums.reduce(fn,init);
 };
