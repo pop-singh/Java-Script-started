e375cd1e35739e65e7bfe0e14d0cbd83567ba424
Pop.singh <31093814+pop-singh@users.noreply.github.com>
2025-09-12 02:47:52 +0530

    Time: 900 ms (9.08%) | Memory: 49.4 MB (14.53%) - LeetSync

--- 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.java
+++ 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.java
@@ -1,24 +1,23 @@
 class Solution {
     public String sortVowels(String s) {
-        String sortedString="";
+        ArrayList<Character> sortedString=new ArrayList<>();
         String result="";
         for(int i=0;i<s.length();i++)
         {
             if(s.charAt(i)=='a'||s.charAt(i)=='e'||s.charAt(i)=='i'||
                   s.charAt(i)=='o'||s.charAt(i)=='u' ||  s.charAt(i)=='A'||s.charAt(i)=='E'||s.charAt(i)=='I' ||s.charAt(i)=='O'||s.charAt(i)=='U' ){
-                    sortedString+=s.charAt(i);
+                    sortedString.add(s.charAt(i));
              
         }
         }
-   char[] sstr=sortedString.toCharArray();
-   Arrays.sort(sstr);
-   System.out.println(sstr);
+  
+   Collections.sort(sortedString);
    int index=0;
     for(int i=0;i<s.length();i++)
         {
             if(s.charAt(i)=='a'||s.charAt(i)=='e'||s.charAt(i)=='i'||
                   s.charAt(i)=='o'||s.charAt(i)=='u' ||  s.charAt(i)=='A'||s.charAt(i)=='E'||s.charAt(i)=='I' ||s.charAt(i)=='O'||s.charAt(i)=='U' ){
-               result+=sstr[index++];     
+               result+=sortedString.get(index++);    
              
         }
         else{
