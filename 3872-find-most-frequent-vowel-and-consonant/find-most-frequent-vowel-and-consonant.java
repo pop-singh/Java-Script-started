0dbd29f181f454de9ad9b0820846c4bad0fce0bb
Pop.singh <31093814+pop-singh@users.noreply.github.com>
2025-09-13 14:43:56 +0530

    Time: 5 ms (18.09%) | Memory: 42.1 MB (96.46%) - LeetSync

--- 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.java
+++ 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.java
@@ -1,26 +1,26 @@
 class Solution {
     public int maxFreqSum(String s) {
-      int count_Vowel[]=new int[26];
-       int count_Consonant[]=new int[26];
-       int max_count=0;
+    
+       int Frequency[]=new int[26];
+       int max_count_consonant=0;
        int max_count_vowel=0;
       for(int i=0;i<s.length();i++)
       {
         if(s.charAt(i)=='a' ||s.charAt(i)=='e' ||s.charAt(i)=='i' ||s.charAt(i)=='o' ||
         s.charAt(i)=='u'){
-            count_Vowel[s.charAt(i)-'a']++;
-           max_count_vowel=Math.max(count_Vowel[s.charAt(i)-'a'],max_count_vowel);
+            Frequency[s.charAt(i)-'a']++;
+           max_count_vowel=Math.max(Frequency[s.charAt(i)-'a'],max_count_vowel);
 
         }
         else{
-           count_Consonant[s.charAt(i)-'a']++;
-           max_count=Math.max(count_Consonant[s.charAt(i)-'a'],max_count);
+            Frequency[s.charAt(i)-'a']++;
+           max_count_consonant=Math.max(Frequency[s.charAt(i)-'a'],max_count_consonant);
         }
       }
      
 
 
-      return (max_count_vowel+max_count);
+      return (max_count_vowel+max_count_consonant);
 
 
 }
